97dc04ad1844f75b46854982c957fabe4244eaf8
szkj-web-he <83932083+szkj-web-he@users.noreply.github.com>
2022-02-21 15:15:42 +0800

    remove log

--- webpack/exclude.js
+++ webpack/exclude.js
@@ -1,8 +1,3 @@
-/**
- * 剔除不必要的打包文件
- * node_modules里非公司库的依赖
- * 本来时用正则  但是担心在mac或者liunx和window上/的通配符不同
- */
 module.exports = /node_modules(\/|\\)(?!@datareachable|@possie-engine)/;
 // /**
 //  * 剔除不必要的打包文件
